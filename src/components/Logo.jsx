bad2a19ecd0893fd016aad76dc2a1b5cde1b57ef
Anders Ravn <anders@adriansen.dk>
2023-04-04 17:54:27 +0200

    Add: Added simple logo component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,14 @@
+import "./Logo.css"
+
+function Logo() {
+  return (
+    <img
+      className="logo"
+      crossOrigin="anonymous"
+      src="https://prod.tradify.dk/content/public/tradify_logo.svg"
+      alt="Tradify Logo"
+    />
+  );
+}
+
+export default Logo;

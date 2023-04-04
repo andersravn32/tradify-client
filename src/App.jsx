1b4700f17102df39c9bfaa4fa0370b2cb04ad339
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-04-04 20:47:16 +0200

    Add: Added ui folder to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import About from "./pages/About";
-import Signin from "./pages/Signin";
-import Signup from "./pages/Signup";
 import Index from "./pages/Index";
-import Profile from "./pages/Profile";
-import DefaultLayout from "./layouts/DefaultLayout";
-import AppLayout from "./layouts/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<DefaultLayout />}>
-          <Route index element={<Index />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/profile/:identifier" element={<Profile />} />
-        </Route>
-        <Route path="/app" element={<AppLayout />}>
-          <Route index element={<Dashboard />} />
-        </Route>
+        <Route index element={<Index />} />
       </Routes>
     </BrowserRouter>
   );

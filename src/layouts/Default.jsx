08f7e1c778379d168cf6b7638f875a3b43b294c6
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-04-05 22:33:29 +0200

    Add: Added Sidenav and Topnav to base layout

--- src/layouts/Default.jsx
+++ src/layouts/Default.jsx
@@ -1,62 +1,22 @@
 import { Outlet, useNavigate } from "react-router-dom";
 import useAuthContext from "../context/AuthContext";
 import { useEffect, useState } from "react";
+import Sidenav from "../components/ui/Sidenav";
+import Topnav from "../components/ui/Topnav";
 
 const AuthContext = useAuthContext();
 
 function DefaultLayout() {
   const [auth, setAuth] = useState(null);
-  const [ready, setReady] = useState(false);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const initialAuthCheck = async () => {
-      if (
-        !(
-          localStorage.getItem("accessToken") ||
-          localStorage.getItem("refreshToken")
-        )
-      ) {
-        return setReady(false);
-      }
-
-      const { data, errors } = await fetch(
-        "https://prod.tradify.dk/auth/refresh",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            token: localStorage.getItem("refreshToken"),
-          }),
-        }
-      ).then((res) => res.json());
-
-      if (errors.length) {
-        navigate("/signin");
-        return setReady(true);
-      }
-
-      localStorage.setItem("accessToken", data.accessToken);
-      localStorage.setItem("refreshToken", data.refreshToken);
-
-      setAuth(data);
-
-      setReady(true);
-    };
-
-    initialAuthCheck();
-  }, [auth, ready]);
-
-  if (!ready) {
-    return <>Loading...</>;
-  }
 
   return (
     <>
       <AuthContext.Provider value={{ auth, setAuth }}>
-        <Outlet />
+        <Sidenav />
+        <Topnav />
+        <main className="pl-64 pt-[72px]">
+          <Outlet />
+        </main>
       </AuthContext.Provider>
     </>
   );

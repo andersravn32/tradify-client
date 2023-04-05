5d4d62215176bf106dd2aa42f150bc12d733b636
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-04-05 22:34:13 +0200

    Reset: Reset index page

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -4,17 +4,10 @@ import useAuthContext from "../context/AuthContext";
 const AuthContext = useAuthContext();
 
 function Index() {
-  const { auth, setAuth } = useContext(AuthContext);
   return (
     <>
-      <div className="bg-slate-50 m-4 p-4 shadow-sm">
-        <h1>Jeg er en header</h1>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae earum
-          odio veritatis dolor nesciunt. Error ratione temporibus, similique
-          veniam magni eius aperiam! Suscipit magni eos eum voluptates vitae
-          deserunt sed!
-        </p>
+      <div className="flex flex-col">
+        
       </div>
     </>
   );

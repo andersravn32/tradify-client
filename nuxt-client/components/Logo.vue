085b8da5cd00c487b751b8d486ba2eeb74d46bd3
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-04-17 09:57:06 +0200

    Add: Added logo component

---
+++ nuxt-client/components/Logo.vue
@@ -0,0 +1,40 @@
+<script setup>
+defineProps({
+    size: {
+        type: String,
+        default: "md"
+    },
+    light: {
+        type: Boolean,
+        required: false,
+        default: false
+    }
+})
+</script>
+
+<template>
+    <img id="logo" :src="light ? '/tradify_logo_light.svg' : '/tradify_logo.svg'" :class="`logo-${size}`"
+        alt="Tradify Logo" />
+</template>
+
+<style>
+.logo-xs {
+    @apply h-8;
+}
+
+.logo-sm {
+    @apply h-12;
+}
+
+.logo-md {
+    @apply h-16;
+}
+
+.logo-lg {
+    @apply h-24;
+}
+
+.logo-xl {
+    @apply h-32;
+}
+</style>

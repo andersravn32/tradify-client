b3c41b639ca6fb2f4dfd7601ae86391091d30294
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-04-17 10:35:05 +0200

    Add: Added navbar

---
+++ nuxt-client/components/Navbar.vue
@@ -0,0 +1,44 @@
+<script setup>
+const router = useRouter();
+
+</script>
+
+<template>
+    <header>
+        <ul>
+            <li>
+                <NuxtLink to="/">
+                    <Logo size="sm" />
+                </NuxtLink>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <NuxtLink class="router-link" to="/about">Om os</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink class="router-link" to="/services">Ydelser</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink class="router-link" to="/signin">Log på</NuxtLink>
+            </li>
+            <li>
+                <Button @click="router.push('/signup')">Opret ny konto</Button>
+            </li>
+        </ul>
+    </header>
+</template>
+
+<style>
+header {
+    @apply p-4 border-b border-b-zinc-800 flex justify-between;
+}
+
+header .router-link {
+    @apply font-semibold;
+}
+
+header ul {
+    @apply flex items-center space-x-16;
+}
+</style>

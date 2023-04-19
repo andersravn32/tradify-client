3d6eee94fad51544d565f18d877c7a4fc7a70eff
Anders Ravn <anders@adriansen.dk>
2023-04-19 13:02:43 +0200

    Add: Added trade counter component

--- nuxt-client/components/TradeCounter.vue
+++ nuxt-client/components/TradeCounter.vue
@@ -4,6 +4,10 @@ const props = defineProps({
     type: Array,
     required: false,
   },
+  userId: {
+    type: String,
+    required: true,
+  },
 });
 
 const counts = ref({
@@ -11,11 +15,41 @@ const counts = ref({
   neutral: 0,
   negative: 0,
 });
+
+props.trades.forEach((trade) => {
+  if (!trade.completed) {
+    return;
+  }
+
+  if (trade.to.uuid == props.userId) {
+    if (trade.to.rating.value == -1) {
+      counts.value.negative++;
+    }
+    if (trade.to.rating.value == 0) {
+      counts.value.neutral++;
+    }
+    if (trade.to.rating.value == 1) {
+      counts.value.positive++;
+    }
+  }
+
+  if (trade.from.uuid == props.userId) {
+    if (trade.from.rating.value == -1) {
+      counts.value.negative++;
+    }
+    if (trade.from.rating.value == 0) {
+      counts.value.neutral++;
+    }
+    if (trade.from.rating.value == 1) {
+      counts.value.positive++;
+    }
+  }
+});
 </script>
 
 <template>
   <div
-    class="w-full grid grid-cols-5 place-items-center bg-zinc-900 p-4 text-lg font-semibold rounded text-zinc-400"
+    class="w-full grid grid-cols-5 place-items-center bg-zinc-900 p-4 font-semibold rounded text-zinc-400"
   >
     <span class="text-green-500">{{ counts.positive }}</span> <span>/</span
     ><span class="text-zinc-100">{{ counts.neutral }}</span

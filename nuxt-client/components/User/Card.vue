3137f8cf3926d3feac64ccde7090491b3d2cbc01
Anders Ravn <anders@adriansen.dk>
2023-04-19 14:24:08 +0200

    Add: Added user card component

--- nuxt-client/components/User/Card.vue
+++ nuxt-client/components/User/Card.vue
@@ -1,9 +1,9 @@
 <script setup>
 import {
-    ArrowLeftOnRectangleIcon,
-    QuestionMarkCircleIcon,
-    UserIcon,
-  } from "@heroicons/vue/24/outline";
+  ArrowLeftOnRectangleIcon,
+  QuestionMarkCircleIcon,
+  UserIcon,
+} from "@heroicons/vue/24/outline";
 import { storeToRefs } from "pinia";
 import useAuthStore from "~/stores/AuthStore";
 
@@ -17,23 +17,39 @@ defineProps({
 // Use auth store
 const authStore = useAuthStore();
 const authStoreRefs = storeToRefs(authStore);
+const router = useRouter();
 </script>
 
 <template>
   <div class="user-card">
-    <div class="mx-auto bg-zinc-700 rounded-full h-32 w-32 bg-cover bg-center">
-      <img
-        class="h-full w-full"
-        crossOrigin="anonymous"
-        :src="`${user.profile.avatar}?token=${authStoreRefs.accessToken.value}`"
-        alt="User avatar"
-      />
-    </div>
+    <UserAvatar
+      class="mx-auto"
+      size="xl"
+      :url="authStoreRefs.user.value.profile.avatar"
+    />
     <div class="flex flex-col items-center">
       <span class="font-semibold text-lg">{{ user.profile.firstName }}</span>
       <span class="text-sm text-zinc-400">@{{ user.identifier }}</span>
       <UserRole :role="user.role" />
     </div>
-    <TradeCounter :trades="user.trades" :userId="user.uuid"/>
+    <TradeCounter :trades="user.trades" :userId="user.uuid" />
+
+    <ul v-if="authStoreRefs.user.value.uuid == user.uuid">
+      <li>
+        <Button
+          type="tertiary"
+          class="flex items-center space-x-2"
+          @click="
+            () => {
+              authStore.signout();
+              router.push('/signin');
+            }
+          "
+          ><ArrowLeftOnRectangleIcon class="h-6 w-6" /><span
+            >Log ud</span
+          ></Button
+        >
+      </li>
+    </ul>
   </div>
 </template>

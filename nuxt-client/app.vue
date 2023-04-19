8439d38d41615c765deef25c2ff830b16074b526
Anders Ravn <anders@adriansen.dk>
2023-04-19 13:03:21 +0200

    Add: Added inital refresh if cookie is set

--- nuxt-client/app.vue
+++ nuxt-client/app.vue
@@ -1,3 +1,14 @@
+<script setup>
+import useAuthStore from "./stores/AuthStore";
+
+const authStore = useAuthStore();
+const refreshToken = useCookie("refreshToken");
+
+if (refreshToken.value) {
+  await authStore.refresh()
+}
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />

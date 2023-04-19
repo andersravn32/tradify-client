7dd3b6dd846ba8c9d606c0c4a5ece5ddd5aab5ae
Anders Ravn <anders@adriansen.dk>
2023-04-19 14:24:18 +0200

    Add: Added topnav

--- nuxt-client/components/Topnav.vue
+++ nuxt-client/components/Topnav.vue
@@ -1,4 +1,12 @@
 <script setup>
+import { storeToRefs } from "pinia";
+import useAuthStore from "~/stores/AuthStore";
+
+// Use auth store
+const authStore = useAuthStore();
+const authStoreRefs = storeToRefs(authStore);
+
+// Use router
 const router = useRouter();
 </script>
 
@@ -9,14 +17,44 @@ const router = useRouter();
         <h2>{{ router.currentRoute.value.name }}</h2>
       </li>
     </ul>
-    <ul>
-      <li><div class="h-10 w-10 bg-zinc-800 rounded-full"></div></li>
+    <ul class="flex items-center space-x-8">
+      <li><Button>Opret handel</Button></li>
+      <li>
+        <div class="user">
+          <div class="flex flex-col text-right">
+            <span class="text-sm font-semibold">{{
+              authStoreRefs.user.value.profile.firstName
+            }}</span>
+            <span class="text-xs text-zinc-400"
+              >@{{ authStoreRefs.user.value.identifier }}</span
+            >
+          </div>
+          <UserAvatar size="sm" :url="authStoreRefs.user.value.profile.avatar" />
+          <UserCard :user="authStoreRefs.user.value"/>
+        </div>
+      </li>
     </ul>
   </nav>
 </template>
 
 <style>
 #topnav {
-  @apply fixed top-0 right-0 left-64 p-4 border-b-2 border-zinc-800 flex justify-between items-center;
+  @apply fixed top-0 right-0 left-64 p-4 border-b-2 border-zinc-800 flex justify-between items-center text-zinc-100;
+}
+
+.user {
+  @apply flex items-center space-x-4 relative;
+}
+
+.user-card {
+  @apply bg-zinc-800 shadow-lg w-64 absolute right-0 top-full flex flex-col p-4 rounded space-y-4;
+}
+
+.user-card .router-link {
+  @apply flex items-center space-x-4 rounded font-semibold text-sm text-zinc-400 hover:text-zinc-100;
+}
+
+.user-card .signout {
+  @apply text-red-500 hover:text-red-600;
 }
 </style>

c08fb0eda5512838e34c7c0c928c3fb4ef72a359
Anders Ravn <anders@adriansen.dk>
2023-04-19 13:02:23 +0200

    Add: Added role pill component

---
+++ nuxt-client/components/User/Role.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  role: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <span
+    v-if="role.permissionLevel == 3"
+    class="border border-indigo-500 rounded-full text-indigo-500 px-3 py-1 text-sm font-semibold"
+    >{{ role.title }}</span
+  >
+  <span
+    v-if="role.permissionLevel == 4"
+    class="border border-amber-500 rounded-full text-amber-500 px-3 py-1 text-sm font-semibold"
+    >{{ role.title }}</span
+  >
+  <span
+    v-if="role.permissionLevel >= 5"
+    class="border border-red-500 rounded-full text-red-500 px-3 py-1 text-sm font-semibold"
+    >{{ role.title }}</span
+  >
+</template>

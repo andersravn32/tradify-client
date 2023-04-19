76678d4c3ce15c8f503b29293052c6fac1ad9e44
Anders Ravn <anders@adriansen.dk>
2023-04-19 09:32:38 +0200

    Add: Scaffolded topnav component

--- nuxt-client/components/Topnav.vue
+++ nuxt-client/components/Topnav.vue
@@ -1,11 +1,22 @@
+<script setup>
+const router = useRouter();
+</script>
+
 <template>
-    <nav id="topnav">
-        <div class="h-10 w-10 bg-zinc-800 rounded-full"></div>
-    </nav>
+  <nav id="topnav">
+    <ul>
+      <li>
+        <h2>{{ router.currentRoute.value.name }}</h2>
+      </li>
+    </ul>
+    <ul>
+      <li><div class="h-10 w-10 bg-zinc-800 rounded-full"></div></li>
+    </ul>
+  </nav>
 </template>
 
 <style>
 #topnav {
-    @apply fixed top-0 right-0 left-64 p-4 border-b-2 border-zinc-800;
+  @apply fixed top-0 right-0 left-64 p-4 border-b-2 border-zinc-800 flex justify-between items-center;
 }
 </style>

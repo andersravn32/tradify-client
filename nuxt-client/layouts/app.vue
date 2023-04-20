345d11cba210c94af30baf24c1a5f7ca23ce5ef6
Anders Ravn <anders@adriansen.dk>
2023-04-20 16:59:13 +0200

    Add: Added base layout styles to app layout

--- nuxt-client/layouts/app.vue
+++ nuxt-client/layouts/app.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="layout-app">
+    <div id="modals"></div>
     <div id="navigation">
       <Sidenav />
       <Topnav />
@@ -9,3 +10,9 @@
     </div>
   </div>
 </template>
+
+<style>
+#content {
+  @apply pl-64 pt-[74px];
+}
+</style>

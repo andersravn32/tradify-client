c6a6e1c55dbb583c0251642de6e6663d10082366
Anders Ravn <anders@adriansen.dk>
2023-04-20 16:57:52 +0200

    Add: Added trades page

---
+++ nuxt-client/pages/trades.vue
@@ -0,0 +1,13 @@
+<script setup>
+definePageMeta({
+  layout: "app",
+  name: "Handler",
+  middleware: "auth",
+});
+</script>
+
+<template>
+    <section id="page-trades">
+        
+    </section>
+</template>

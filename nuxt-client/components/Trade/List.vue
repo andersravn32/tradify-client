4929c9b9c53347fa7260d6f897d4572aa187b292
Anders Ravn <anders@adriansen.dk>
2023-04-20 17:07:01 +0200

    Add: Scaffolded trade list

---
+++ nuxt-client/components/Trade/List.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps({
+  trades: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<template></template>
--- nuxt-client/pages/trades.vue
+++ nuxt-client/pages/trades.vue
@@ -1,13 +1,18 @@
 <script setup>
+import { storeToRefs } from "pinia";
+import useAuthStore from "~/stores/AuthStore";
+
 definePageMeta({
   layout: "app",
   name: "Handler",
   middleware: "auth",
 });
+
+const authStore = useAuthStore();
 </script>
 
 <template>
-    <section id="page-trades">
-        
-    </section>
+  <section id="page-trades">
+    <pre v-for="trade in storeToRefs(authStore).user.value.trades">{{ JSON.stringify(trade) }}</pre>
+  </section>
 </template>

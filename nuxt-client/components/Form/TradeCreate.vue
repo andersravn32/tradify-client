696dd89baa764c2456a956081c1cec9b4b19460f
Anders Ravn <anders@adriansen.dk>
2023-04-20 16:59:03 +0200

    Add: added form for creating trades and searching for users

---
+++ nuxt-client/components/Form/TradeCreate.vue
@@ -0,0 +1,91 @@
+<script setup>
+import { storeToRefs } from "pinia";
+import useAuthStore from "~/stores/AuthStore";
+
+const authStore = useAuthStore();
+const authStoreRefs = storeToRefs(authStore);
+
+const props = defineProps({
+  user: {
+    type: Object,
+    default: null,
+  },
+});
+
+const trade = ref({
+  to: {
+    uuid: props.user,
+  },
+  middleman: {
+    uuid: null,
+  },
+  title: null,
+  description: null,
+});
+
+const loading = ref(false);
+
+const create = async () => {
+  // Update loading state
+  loading.value = true;
+
+  const { data, errors } = await fetch("https://prod.tradify.dk/trades", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: authStoreRefs.accessToken.value,
+    },
+    body: JSON.stringify(trade.value),
+  }).then((res) => res.json());
+
+  console.log(data, errors);
+  loading.value = false;
+};
+</script>
+
+<template>
+  <form id="form-trade-create" @submit.prevent="create">
+    <h2 class="text-2xl text-zinc-50">Opret handel</h2>
+    <div class="flex flex-col space-y-2">
+      <label for="user">Modtager</label>
+      <FormUserSearch @done="(e) => (trade.to.uuid = e.uuid)" />
+    </div>
+
+    <Input
+      type="text"
+      placeholder="Indtast titel"
+      label="Titel"
+      @input="(e) => (trade.title = e.value)"
+    />
+
+    <div class="flex flex-col space-y-2">
+      <label for="description">Beskrivelse</label>
+      <textarea
+        v-model="trade.description"
+        class="px-6 py-2 bg-zinc-800 rounded outline-indigo-500 text-zinc-50 h-32 text-sm resize-none"
+        placeholder="Indtast beskrivelse"
+      ></textarea>
+    </div>
+    <p class="text-xs text-zinc-400">
+      Ved oprette denne handel acceptere du at denne handel er blevet oprette og
+      ikke kan annuleres igen jævnfør Tradify's
+      <a class="text-indigo-500 hover:text-indigo-600" href="#"
+        >vilkår og betingelser</a
+      >
+    </p>
+    <div class="flex items-center justify-between">
+      <Button @click.prevent type="tertiary">Annuller</Button>
+      <Button :loading="loading">Opret handel</Button>
+    </div>
+  </form>
+</template>
+
+<style>
+#form-trade-create {
+  @apply w-full max-w-md flex flex-col space-y-4 p-4;
+}
+
+#form-trade-create label {
+  @apply text-zinc-400 uppercase text-sm;
+}
+</style>
---
+++ nuxt-client/components/Form/UserSearch.vue
@@ -0,0 +1,78 @@
+<script setup>
+import { storeToRefs } from "pinia";
+import useAuthStore from "~/stores/AuthStore";
+
+const authStore = useAuthStore();
+const authStoreRefs = storeToRefs(authStore);
+
+const results = ref([]);
+
+const selected = ref(null);
+
+defineEmits(["done", "reset"]);
+
+const search = async (e) => {
+  if (!e.value) {
+    return;
+  }
+
+  const { data, errors } = await fetch(
+    `https://prod.tradify.dk/users/${e.value}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: authStoreRefs.accessToken.value,
+      },
+    }
+  ).then((res) => res.json());
+
+  if (!data || !data.length) {
+    return;
+  }
+
+  results.value = data.filter((user) => {
+    return user.uuid != authStoreRefs.user.value.uuid;
+  });
+};
+</script>
+
+<template>
+  <div id="form-search">
+    <div v-if="!selected" class="flex flex-col space-y-4">
+      <Input
+        type="text"
+        placeholder="Indtast brugernavn eller e-mail"
+        @input="search"
+      />
+      <ul v-if="results.length" class="flex flex-col space-y-2">
+        <li v-for="user in results" class="flex items-center justify-between">
+          <UserSmall :user="user" />
+          <Button
+            type="primary"
+            size="sm"
+            @click="
+              results = [];
+              selected = user;
+              $emit('done', user);
+            "
+            >Vælg</Button
+          >
+        </li>
+      </ul>
+    </div>
+    <div v-if="selected" class="flex items-center justify-between">
+      <UserSmall :user="selected" />
+      <Button
+        type="secondary"
+        size="sm"
+        @click="
+          selected = null;
+          $emit('reset');
+        "
+      >
+        Slet
+      </Button>
+    </div>
+  </div>
+</template>

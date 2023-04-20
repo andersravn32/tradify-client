be288ff32f4f3ef55834316c2be0f517bac10057
Anders Ravn <anders@adriansen.dk>
2023-04-20 16:58:15 +0200

    Add: Added overlay as component

---
+++ nuxt-client/components/Overlay.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="overlay">
+    <slot />
+  </div>
+</template>
+
+<style>
+.overlay {
+  @apply fixed top-0 left-0 right-0 bottom-0 bg-zinc-900/30 z-10 cursor-default;
+}
+</style>

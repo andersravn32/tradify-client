8da05d9d4bef2ddc4a7a39ce49efe74f917434da
Anders Ravn <anders@adriansen.dk>
2023-04-21 09:55:21 +0200

    Add: Added better pages structuring

---
+++ nuxt-client/pages/profile/[identifier].vue
@@ -0,0 +1,14 @@
+<script setup>
+definePageMeta({
+  layout: "app",
+  name: "Profil",
+  middleware: "auth",
+});
+</script>
+
+
+<template>
+    <section id="page-profile">
+        
+    </section>
+</template>
---
+++ nuxt-client/pages/profile/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { storeToRefs } from "pinia";
+import useAuthStore from "~/stores/AuthStore";
+
+definePageMeta({
+  layout: "app",
+  name: "Min profil",
+  middleware: "auth",
+});
+
+const authStore = useAuthStore();
+</script>
+
+<template>
+  <section id="page-profile">
+    <UserProfile :user="storeToRefs(authStore).user.value" />
+  </section>
+</template>

0d1f8f872928ece96d79d6e331aa84e1ae3dba2f
Anders Ravn <anders@adriansen.dk>
2023-04-21 10:10:19 +0200

    Add: Added runtime config object

--- nuxt-client/nuxt.config.ts
+++ nuxt-client/nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  runtimeConfig: {
+    backendUrl: "https://prod.tradify.dk",
+  },
 });

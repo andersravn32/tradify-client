dbe59f954e978090963a664ffe941a9363e14cf5
Anders Ravn <anders@adriansen.dk>
2023-04-21 09:55:32 +0200

    Add: Added user cover component

---
+++ nuxt-client/components/User/Cover.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { storeToRefs } from "pinia";
+import useAuthStore from "~/stores/AuthStore";
+
+const authStore = useAuthStore();
+const authStoreRefs = storeToRefs(authStore);
+
+defineProps({
+  url: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="user-cover">
+    <img
+      class="h-full w-full object-cover"
+      crossorigin="anonymous"
+      :src="`${url}?token=${authStoreRefs.accessToken.value}`"
+      alt="User avatar"
+    />
+    <slot />
+  </div>
+</template>
+
+<style>
+.user-cover {
+  @apply w-full relative;
+}
+</style>

52b73dc26408ae4642352c87b6fdb677084f9d95
Anders Ravn <anders@adriansen.dk>
2023-04-21 11:13:06 +0200

    Add: Added wrapper div to role

--- nuxt-client/components/User/Role.vue
+++ nuxt-client/components/User/Role.vue
@@ -8,19 +8,21 @@ defineProps({
 </script>
 
 <template>
-  <span
-    v-if="role.permissionLevel == 3"
-    class="border border-indigo-500 rounded-full text-indigo-500 px-3 py-1 text-sm font-semibold"
-    >{{ role.title }}</span
-  >
-  <span
-    v-if="role.permissionLevel == 4"
-    class="border border-amber-500 rounded-full text-amber-500 px-3 py-1 text-sm font-semibold"
-    >{{ role.title }}</span
-  >
-  <span
-    v-if="role.permissionLevel >= 5"
-    class="border border-red-500 rounded-full text-red-500 px-3 py-1 text-sm font-semibold"
-    >{{ role.title }}</span
-  >
+  <div>
+    <span
+      v-if="role.permissionLevel == 3"
+      class="border border-indigo-500 rounded-full text-indigo-500 px-3 py-1 text-sm font-semibold"
+      >{{ role.title }}</span
+    >
+    <span
+      v-if="role.permissionLevel == 4"
+      class="border border-amber-500 rounded-full text-amber-500 px-3 py-1 text-sm font-semibold"
+      >{{ role.title }}</span
+    >
+    <span
+      v-if="role.permissionLevel >= 5"
+      class="border border-red-500 rounded-full text-red-500 px-3 py-1 text-sm font-semibold"
+      >{{ role.title }}</span
+    >
+  </div>
 </template>

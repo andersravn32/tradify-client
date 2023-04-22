cb601f693ff5c99932f99c185a9b5a08fa81e272
Anders Ravn <anders@adriansen.dk>
2023-04-22 11:18:21 +0200

    Add: Added wrapper div

--- nuxt-client/components/TradeCounter.vue
+++ nuxt-client/components/TradeCounter.vue
@@ -24,23 +24,25 @@ const counts = ref({
 </script>
 
 <template>
-  <div
-    v-if="size == 'md'"
-    class="w-full grid grid-cols-5 place-items-center bg-zinc-900 p-4 font-semibold rounded text-zinc-400"
-  >
-    <span class="text-green-500">{{ counts.positive }}</span> <span>/</span
-    ><span class="text-zinc-100">{{ counts.neutral }}</span
-    ><span>/</span>
-    <span class="text-red-500">{{ counts.negative }}</span>
-  </div>
+  <div>
+    <div
+      v-if="size == 'md'"
+      class="w-full grid grid-cols-5 place-items-center bg-zinc-900 p-4 font-semibold rounded text-zinc-400"
+    >
+      <span class="text-green-500">{{ counts.positive }}</span> <span>/</span
+      ><span class="text-zinc-100">{{ counts.neutral }}</span
+      ><span>/</span>
+      <span class="text-red-500">{{ counts.negative }}</span>
+    </div>
 
-  <div
-    v-if="size == 'sm'"
-    class="text-zinc-400 grid grid-cols-5 place-items-center font-semibold"
-  >
-    <span class="text-green-500">{{ counts.positive }}</span
-    ><span>/</span><span class="text-zinc-100">{{ counts.neutral }}</span
-    ><span>/</span>
-    <span class="text-red-500">{{ counts.negative }}</span>
+    <div
+      v-if="size == 'sm'"
+      class="text-zinc-400 grid grid-cols-5 place-items-center font-semibold"
+    >
+      <span class="text-green-500">{{ counts.positive }}</span
+      ><span>/</span><span class="text-zinc-100">{{ counts.neutral }}</span
+      ><span>/</span>
+      <span class="text-red-500">{{ counts.negative }}</span>
+    </div>
   </div>
 </template>

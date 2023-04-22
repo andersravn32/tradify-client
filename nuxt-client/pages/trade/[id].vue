152fe8f9e734a6d692fb99f28f2f815ea2d8d138
Anders Ravn <anders@adriansen.dk>
2023-04-22 12:10:18 +0200

    Add: Added trade page

--- nuxt-client/pages/trade/[id].vue
+++ nuxt-client/pages/trade/[id].vue
@@ -1,11 +1,34 @@
 <script setup>
+import { storeToRefs } from 'pinia';
+import useAuthStore from '~/stores/AuthStore';
+
 definePageMeta({
   layout: "app",
   name: "Handel",
   middleware: "auth",
 });
+
+const runtimeConfig = useRuntimeConfig();
+const authStore = useAuthStore();
+const router = useRouter();
+const modal = useModal();
+
+const { data, error } = await fetch(
+  `${runtimeConfig.public.backendUrl}/trade/${router.currentRoute.value.params.id}`,
+  {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: storeToRefs(authStore).accessToken.value,
+    },
+  }
+).then((res) => res.json());
+
+const trade = ref(data);
 </script>
 
 <template>
-  <section id="page-trade"></section>
+  <section id="page-trade">
+    <TradeHeader :trade="trade" />
+  </section>
 </template>

7d1b8aeafecd5f4f67e9efa10854de9147426a63
Anders Ravn <anders@adriansen.dk>
2023-04-22 16:34:59 +0200

    Add: Added user based on rtl prop instead of css

--- nuxt-client/components/Topnav.vue
+++ nuxt-client/components/Topnav.vue
@@ -26,7 +26,7 @@ const router = useRouter();
       </li>
       <li class="text-zinc-700">|</li>
       <li>
-        <UserSmall :user="authStoreRefs.user.value" />
+        <UserSmall direction="rtl" :user="authStoreRefs.user.value" />
       </li>
     </ul>
   </nav>
@@ -35,29 +35,5 @@ const router = useRouter();
 <style>
 #topnav {
   @apply fixed top-0 right-0 left-80 p-4 border-b-2 border-zinc-800 flex justify-between items-center text-zinc-100 z-10;
-}
-
-#topnav .user {
-  @apply flex-row-reverse space-x-0;
-}
-
-#topnav .user .user-avatar {
-  @apply ml-4;
-}
-
-#topnav .user-card .user-avatar {
-  @apply ml-auto;
-}
-
-#topnav .user .user-details {
-  @apply text-right;
-}
-
-#topnav .user .user-details span:nth-of-type(1) {
-  @apply flex-row-reverse space-x-0;
-}
-
-#topnav .user .user-details span:nth-of-type(1) span {
-  @apply ml-2;
 }
 </style>

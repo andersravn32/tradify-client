0cb0286916d2233ea09882c847c4575efb096504
Anders Ravn <anders@adriansen.dk>
2023-04-22 14:33:57 +0200

    Add: Added slight styling of list and list items

--- nuxt-client/components/Trade/List.vue
+++ nuxt-client/components/Trade/List.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <ul class="trade-list">
-    <li class="text-sm uppercase text-zinc-400 grid grid-cols-5 gap-4">
+    <li class="text-sm uppercase text-zinc-400 grid grid-cols-5 gap-4 p-4 border-b-2 border-zinc-800">
       <span>Titel</span><span>Kategori</span><span>Afsender</span
       ><span>Modtager</span><span>Handling</span>
     </li>
@@ -19,6 +19,14 @@ defineProps({
 
 <style>
 .trade-list {
-  @apply flex flex-col space-y-4;
+  @apply flex flex-col;
+}
+
+.trade-list-item {
+  @apply p-4;
+}
+
+.trade-list-item:nth-of-type(odd){
+  @apply bg-zinc-800/25 p-4;
 }
 </style>

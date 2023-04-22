e179a7ea380dc254cf2a609aa5069a26a5531a8d
Anders Ravn <anders@adriansen.dk>
2023-04-22 12:00:56 +0200

    Add: Added modal and modaloverlay component

---
+++ nuxt-client/components/Modal.vue
@@ -0,0 +1,26 @@
+<script setup>
+const modal = useModal();
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="modal" :class="{'modal-hidden': id != modal.currentModal}">
+    <slot />
+  </div>
+</template>
+
+<style>
+.modal{
+    @apply bg-zinc-800 rounded shadow-lg;
+}
+
+.modal-hidden {
+    @apply hidden;
+}
+</style>
---
+++ nuxt-client/components/ModalOverlay.vue
@@ -0,0 +1,18 @@
+<script setup>
+const modal = useModal();
+const slots = useSlots()
+console.log()
+provide("modal", modal.value.currentModal);
+</script>
+
+<template>
+  <div v-if="modal.show" id="modals">
+    <slot />
+  </div>
+</template>
+
+<style>
+#modals {
+    @apply bg-zinc-900/30 z-20 fixed top-0 left-0 right-0 bottom-0 grid place-items-center;
+}
+</style>

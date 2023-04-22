2b29366c75a24d4b9bf6027d7826ecc33924aa3a
Anders Ravn <anders@adriansen.dk>
2023-04-22 16:34:19 +0200

    Add: Added user direction prop

--- nuxt-client/components/User/Small.vue
+++ nuxt-client/components/User/Small.vue
@@ -1,42 +1,72 @@
 <script setup>
 import { CheckBadgeIcon } from "@heroicons/vue/24/solid";
-import useDataStore from "~/stores/DataStore";
+
 defineProps({
   user: {
     type: Object,
     required: true,
   },
+  direction: {
+    type: String,
+    default: "ltr",
+  },
 });
 
-const modal = useModal();
 const showUserCard = ref(false);
-const handleClick = (e) => {
-  if (e.target.className != "overlay") {
+
+const displayUserCard = (e) => {
+  if (!showUserCard.value && e.target.className != "overlay") {
     showUserCard.value = true;
+  }
+};
+
+const hideUserCard = (e) => {
+  if (showUserCard.value) {
+    showUserCard.value = false;
   }
 };
 </script>
 
 <template>
-  <div class="user" @click="handleClick">
-    <UserAvatar size="sm" :url="user.profile.avatar" />
-    <div class="user-details">
-      <span class="text-sm font-semibold text-zinc-50 flex items-center space-x-2">
-        <span>{{ user.profile.firstName }}</span><CheckBadgeIcon v-if="user.verified" class="h-5 w-5 text-sky-500"
+  <div class="user">
+    <div
+      @click="displayUserCard"
+      class="flex items-center space-x-4 cursor-pointer"
+    >
+      <UserAvatar
+        v-if="direction == 'ltr'"
+        size="sm"
+        :url="user.profile.avatar"
       />
-      </span>
-      <span class="text-xs text-zinc-400"> @{{ user.identifier }} </span>
+      <div class="user-details" :class="`user-details-${direction}`">
+        <span
+          class="text-sm font-semibold text-zinc-50 flex items-center space-x-2"
+        >
+          <span>{{ user.profile.firstName }}</span
+          ><CheckBadgeIcon v-if="user.verified" class="h-5 w-5 text-sky-500" />
+        </span>
+        <span class="text-xs text-zinc-400"> @{{ user.identifier }} </span>
+      </div>
+      <UserAvatar
+        v-if="direction == 'rtl'"
+        size="sm"
+        :url="user.profile.avatar"
+      />
     </div>
-    <UserCard v-if="showUserCard" @close="showUserCard = false; modal.currentModal = ''; modal.show = false;" :user="user" />
+    <UserCard v-if="showUserCard" @close="hideUserCard" :user="user" />
   </div>
 </template>
 
 <style>
 .user {
-  @apply flex items-center space-x-4 relative cursor-pointer;
+  @apply relative;
 }
 
-.user .user-details{
-    @apply flex flex-col;
+.user .user-details {
+  @apply flex flex-col;
+}
+
+.user .user-details-rtl {
+  @apply text-right;
 }
 </style>

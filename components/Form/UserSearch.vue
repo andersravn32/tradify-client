b5afb807a62ca49f8bda1131a7af5deb1fe9847f
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-04-23 19:35:48 +0200

    Add: Added resultscounter

--- components/Form/UserSearch.vue
+++ components/Form/UserSearch.vue
@@ -67,7 +67,7 @@ const search = async (e) => {
         placeholder="Indtast brugernavn eller e-mail"
         @input="handleInput"
       />
-      <ul v-if="results.length" class="flex flex-col space-y-2">
+      <ul v-if="results.length" class="flex flex-col space-y-2 max-h-[128px] overflow-scroll">
         <li
           v-for="user in results"
           class="flex items-center justify-between p-2 rounded odd:bg-zinc-900/25"
@@ -85,6 +85,7 @@ const search = async (e) => {
           >
         </li>
       </ul>
+      <p class="text-zinc-400 text-xs">Fandt {{ results.length }} {{ results.length == 1 ? 'resultat': 'resultater'}}</p>
     </div>
     <div v-if="selected" class="flex items-center justify-between">
       <UserSmall :user="selected" />

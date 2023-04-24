566a393055c5545a19ebd3b09d64676844f22e79
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-04-24 10:17:29 +0200

    Refactor: Refactored TradeInformation component

--- components/Trade/Information.vue
+++ components/Trade/Information.vue
@@ -11,20 +11,24 @@ defineProps({
     <div class="trade-information">
         <div class="flex justify-between">
             <div class="flex flex-col space-y-2">
-                <span class="uppercase text-zinc-400 text-sm">Titel</span>
+                <span class="uppercase text-zinc-400">Titel</span>
                 <h3 class="text-zinc-50">{{ trade.title }}</h3>
             </div>
             <div class="flex flex-col space-y-2">
-                <span class="uppercase text-zinc-400 text-sm">Dato</span>
-                <span class="text-zinc-50 text-sm">{{ trade.date.split("T")[0] }}</span>
+                <span class="uppercase text-zinc-400">Oprettelses Dato</span>
+                <span class="text-zinc-50">{{ trade.date.split("T")[0] }}</span>
             </div>
         </div>
         <div class="flex flex-col space-y-2">
-            <span class="text-zinc-400 uppercase text-sm">Beskrivelse</span>
-            <p class="text-zinc-50 text-sm">{{ trade.description }}</p>
+            <span class="text-zinc-400 uppercase">Kategori</span>
+            <p class="text-zinc-50">{{ trade.category ? trade.category : '-' }}</p>
+        </div>
+        <div class="flex flex-col space-y-2">
+            <span class="text-zinc-400 uppercase">Beskrivelse</span>
+            <p class="text-zinc-50">{{ trade.description }}</p>
         </div>
         <div class="flex flex-col space-y-2" v-if="trade.middleman.uuid">
-            <span class="text-zinc-400 uppercase text-sm">Mellemmand</span>
+            <span class="text-zinc-400 uppercase">Mellemmand</span>
             <UserSmall :user="trade.middleman" />
         </div>
     </div>
@@ -32,6 +36,6 @@ defineProps({
 
 <style>
     .trade-information {
-        @apply flex flex-col space-y-4;
+        @apply flex flex-col space-y-4 text-sm;
     }
 </style>

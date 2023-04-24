6a43a63a880ef1f9713d9495e0a1c021e1943555
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-04-24 09:19:37 +0200

    Add: Added tab and tabs component

---
+++ components/Tab.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+    title: {
+        required: true
+    }
+})
+
+const selectedTitle = inject("selectedTitle");
+</script>
+
+<template>
+    <div v-if="selectedTitle == title" class="tab">
+        <slot />
+    </div>
+</template>
---
+++ components/Tabs.vue
@@ -0,0 +1,20 @@
+<script setup>
+const slots = useSlots();
+const tabs = slots.default();
+
+const selectedTitle = ref(tabs[0].props.title);
+
+provide("selectedTitle", selectedTitle)
+</script>
+
+<template>
+    <div class="tabs">
+        <ul class="tabs-titles">
+            <li v-for="tab in tabs" @click="selectedTitle = tab.props.title" class="tab-title"
+                :class="{ 'tab-title-active': selectedTitle == tab.props.title }">
+                {{ tab.props.title }}
+            </li>
+        </ul>
+        <slot />
+    </div>
+</template>

82ce67955bbf6feed8b2c1e703411d8a22a2b9d2
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-04-24 10:20:08 +0200

    Add: Added solid default cover image

--- components/User/Cover.vue
+++ components/User/Cover.vue
@@ -15,13 +15,8 @@ defineProps({
 
 <template>
   <div class="user-cover">
-    <img
-      v-if="url"
-      class="h-full w-full object-cover"
-      crossorigin="anonymous"
-      :src="`${url}?token=${authStoreRefs.accessToken.value}`"
-      alt="User avatar"
-    />
+    <img v-if="url" class="h-full w-full object-cover" crossorigin="anonymous"
+      :src="`${url}?token=${authStoreRefs.accessToken.value}`" alt="User avatar" />
     <div v-if="!url" class="h-full w-full object-cover user-cover-standin">
     </div>
     <slot />
@@ -33,7 +28,7 @@ defineProps({
   @apply w-full relative;
 }
 
-.user-cover-standin{
-  @apply bg-gradient-to-tr from-indigo-700 to-indigo-900;
+.user-cover-standin {
+  @apply bg-indigo-500;
 }
 </style>

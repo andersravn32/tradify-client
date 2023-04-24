a9359d4fc40cf78a8bf06438d6d396610b7abde9
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-04-24 10:17:12 +0200

    Add: Added default tab styling

--- components/Tabs.vue
+++ components/Tabs.vue
@@ -18,3 +18,21 @@ provide("selectedTitle", selectedTitle)
         <slot />
     </div>
 </template>
+
+<style>
+.tabs-titles {
+    @apply flex items-center space-x-4 p-4 border-b-2 border-zinc-800;
+}
+
+.tab-title {
+    @apply text-sm uppercase text-zinc-400 cursor-pointer hover:text-zinc-50;
+}
+
+.tab-title-active {
+    @apply text-zinc-50;
+}
+
+.tab {
+    @apply flex flex-col p-4;
+}
+</style>

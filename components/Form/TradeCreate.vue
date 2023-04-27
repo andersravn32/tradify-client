89f49fcc7dba98b9743f3c4248cbf62fd901d673
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-04-27 09:34:58 +0200

    Fix: Fixed tradeCreate form

--- components/Form/TradeCreate.vue
+++ components/Form/TradeCreate.vue
@@ -7,6 +7,7 @@ import useUserStore from "~/stores/UserStore";
 
 const authStore = useAuthStore();
 const notificationStore = useNotificationStore();
+const tradeStore = useTradeStore();
 const runtimeConfig = useRuntimeConfig();
 const userStore = useUserStore();
 const modal = useModal();
@@ -57,8 +58,14 @@ const create = async () => {
     return;
   }
 
+  // Reset modal
   modal.value.currentModal = "";
   modal.value.show = false;
+
+  // Push trade to tradeStore data
+  trade.value = await tradeStore.load(data._id)
+  storeToRefs(authStore).user.value.trades.push(trade.value);
+
   notificationStore.add("info", {
     msg: `Du har nu oprettet en ny handel: ${data._id}`,
   });
@@ -77,52 +84,32 @@ onUnmounted(() => {
       <label for="user">Modtager</label>
       <div v-if="trade.to.uuid" class="flex items-center justify-between">
         <User :user="trade.to" />
-        <Button type="tertiary" @click.prevent="trade.to.uuid = null"
-          >Slet</Button
-        >
+        <Button type="tertiary" @click.prevent="trade.to.uuid = null">Slet</Button>
       </div>
-      <FormUserSearch
-        v-if="!trade.to.uuid"
-        @done="
-          (e) => {
-            trade.to = e;
-          }
-        "
-      />
+      <FormUserSearch v-if="!trade.to.uuid" @done="(e) => {
+          trade.to = e;
+        }
+        " />
     </div>
 
-    <Input
-      type="text"
-      placeholder="Indtast titel"
-      label="Titel"
-      @input="(e) => (trade.title = e.value)"
-    />
+    <Input type="text" placeholder="Indtast titel" label="Titel" @input="(e) => (trade.title = e.value)" />
 
     <div class="flex flex-col space-y-2">
       <label for="description">Beskrivelse</label>
-      <textarea
-        v-model="trade.description"
+      <textarea v-model="trade.description"
         class="px-6 py-2 bg-zinc-800 rounded outline-indigo-500 text-zinc-50 h-32 text-sm resize-none"
-        placeholder="Indtast beskrivelse"
-      ></textarea>
+        placeholder="Indtast beskrivelse"></textarea>
     </div>
     <p class="text-xs text-zinc-400">
       Ved oprette denne handel acceptere du at denne handel er blevet oprette og
       ikke kan annuleres igen jævnfør Tradify's
-      <a class="text-indigo-500 hover:text-indigo-600" href="#"
-        >vilkår og betingelser</a
-      >
+      <a class="text-indigo-500 hover:text-indigo-600" href="#">vilkår og betingelser</a>
     </p>
     <div class="flex items-center justify-between">
-      <Button
-        @click.prevent="
-          modal.currentModal = '';
-          modal.show = false;
-        "
-        type="tertiary"
-        >Annuller</Button
-      >
-      <Button :loading="loading">Opret handel</Button>
+      <Button @click.prevent="modal.currentModal = '';
+      modal.show = false;
+                                " type="tertiary">Annuller</Button>
+      <Button :loading=" loading ">Opret handel</Button>
     </div>
   </form>
 </template>

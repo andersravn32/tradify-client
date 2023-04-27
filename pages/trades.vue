3396479b275bedfbcfb608b4d64792dc1dcd9f11
Anders Ravn <71639005+andersravn32@users.noreply.github.com>
2023-04-27 09:33:34 +0200

    Fix: Fixed trades page reactivity

--- pages/trades.vue
+++ pages/trades.vue
@@ -15,19 +15,24 @@ const authStore = useAuthStore();
   <section id="page-trades">
     <Tabs>
       <Tab title="Anmodninger">
-        <TradeList
-          title="Anmodninger"
-          :trades="storeToRefs(authStore).user.value.trades"
-        />
+        <TradeList title="Anmodninger" :trades="storeToRefs(authStore).user.value.trades.filter((trade) => {
+          return trade.to.uuid == storeToRefs(authStore).user.value.uuid && !trade.to.confirmed && !trade.completed
+        })" />
       </Tab>
       <Tab title="Sendte">
-        <TradeList title="Sendte" :trades="[]" />
+        <TradeList title="Sendte" :trades="storeToRefs(authStore).user.value.trades.filter((trade) => {
+            return trade.from.uuid == storeToRefs(authStore).user.value.uuid && !trade.to.confirmed && !trade.completed
+          })" />
       </Tab>
       <Tab title="Igangværende">
-        <TradeList title="Igangværende" :trades="[]" />
+        <TradeList title="Igangværende" :trades="storeToRefs(authStore).user.value.trades.filter((trade) => {
+            return trade.from.confirmed && trade.to.confirmed && !trade.completed
+          })" />
       </Tab>
       <Tab title="Afsluttede">
-        <TradeList title="Afsluttede" :trades="[]" />
+        <TradeList title="Afsluttede" :trades="storeToRefs(authStore).user.value.trades.filter((trade) => {
+            return trade.from.confirmed && trade.to.confirmed && trade.completed
+          })" />
       </Tab>
     </Tabs>
   </section>

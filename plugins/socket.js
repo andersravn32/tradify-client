fc4f7adce62dc3fd33d9ade2b3f1e191077e53d7
Anders Ravn <anders@adriansen.dk>
2023-04-28 14:58:52 +0200

    Add: Added self contained plugin state

--- plugins/socket.js
+++ plugins/socket.js
@@ -3,10 +3,14 @@ import { io } from "socket.io-client";
 export default defineNuxtPlugin(() => {
   const runtimeConfig = useRuntimeConfig();
   const socket = io(runtimeConfig.public.backendUrl);
+  const state = ref({
+    ready: false
+  })
 
   return {
     provide: {
-      socket: socket
+      socket: socket,
+      state: state
     },
   };
 });
